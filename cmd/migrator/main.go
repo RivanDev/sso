e9a809849f4961a3e0fcf5ef72d356c011732f81
rivan <rivan_git@mail.ru>
2024-09-06 14:58:52 +0300

    add migrator

---
+++ cmd/migrator/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	// Библиотека для миграций
+	"github.com/golang-migrate/migrate/v4"
+	// Драйвер для выполнения миграций
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	// Драйвер для получения миграций из файлов
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storagePath, "storage-path", "", "Path to a directory containing migration files")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "Path to a directory containing migration files")
+	flag.StringVar(&migrationsTable, "migrations-table", "", "Path to a table containing migration files")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied successfully")
+}
